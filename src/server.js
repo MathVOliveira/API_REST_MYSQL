fc5448c7a0b4643cbc6bf0bcb4b0edb4af72a129
Matheus Viana de Oliveira <matheus.viana@hotmail.com>
2024-09-21 02:54:45 -0300

    rotas separadas para arquivo routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from "./app.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-app.listen(process.env.APP_PORT, () => {
+app.listen(process.env.APP_PORT || 3000, () => {
   console.log(`Servidor rodando em: http://localhost:${process.env.APP_PORT}`);
 })
